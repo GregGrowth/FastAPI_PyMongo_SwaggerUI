888d2d72b5d2d5b350c4cf0bc42388d7ac9785ec
GREGOIRE DELCROIX <gregoiredelcroix@MacBook-Pro-de-GREGOIRE-3.local>
2024-07-23 17:29:08 +0200

    ajout_démarrer_application

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from route import classe_route, eleve_route, matiere_route, note_route,professeur_route, trimestre_route
-
+from route import classe_route, eleve_route, matiere_route, note_route, professeur_route, trimestre_route
+import uvicorn
 
 # Initialiation de l'application avec FastAPI
 app = FastAPI()
@@ -10,3 +10,7 @@ app.include_router(matiere_route.router)
 app.include_router(note_route.router)
 app.include_router(professeur_route.router)
 app.include_router(trimestre_route.router)
+
+# Démarrer l'application avec uvicorn
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
