314b1baced0c6e5919d5a37f438928f86d7d3dce
mkunegel <melissa.kunegel@gmail.com>
2024-07-26 10:00:47 +0200

    modif length ideleve (cf. eleve_schema.py)

--- schema/eleve_schema.py
+++ schema/eleve_schema.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 # Schema pour structurer les donnees pour la fonction Insert
 class EleveInsertSchema(BaseModel):
-    id: str = constr(min_length=1, max_length=1)
+    id: str = constr(min_length=1, max_length=3)
     nom: str = constr(min_length=1)
     prenom: str = constr(min_length=1)
     classe: str = constr(min_length=1, max_length=1)
